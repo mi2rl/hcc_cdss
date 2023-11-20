292b07e2ae8011c240224ac42ae8978b83cfb86e
kh12lee <62242917+kh12lee@users.noreply.github.com>
2023-11-20 21:23:56 +0900

    Update Risk_stratification.py

--- Risk_stratification.py
+++ Risk_stratification.py
@@ -19,15 +19,9 @@ from sksurv.metrics import integrated_brier_score
 
 
 # Dataset loading
-df = pd.read_excel('/Nas/0_All_rev.xlsx', 
-                   sheet_name='All', engine='openpyxl')
+df = pd.read_excel('./Dataset.xlsx', sheet_name='Sheet1', engine='openpyxl')
 
-df = df.fillna(0)
-
-for i in df.columns[1:-1]:
-    for j in range(len(df)):
-        df.loc[j,i] = float(df.loc[j,i])
-
+# 9 institutional datasets
 df_KU = df[df['Center'] == 1]
 df_BH = df[df['Center'] == 2]
 df_SM = df[df['Center'] == 3]
